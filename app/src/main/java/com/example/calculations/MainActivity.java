a399e2b08db4f0719fdb8965aa5c46d180db1f67
QV1LL <ghost228678@gmail.com>
2024-08-03 23:57:06 +0300

    Start develop class Tank

--- app/src/main/java/com/example/calculations/MainActivity.java
+++ app/src/main/java/com/example/calculations/MainActivity.java
@@ -6,6 +6,8 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -21,15 +23,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
     private static String mainTag = "technical";
 
     private ArrayList<Tank> tanks = new ArrayList<Tank>();
-    private ArrayList<View> popupMenuItems = new ArrayList<View>();
+    private ArrayList<MenuItem> popupMenuItems = new ArrayList<MenuItem>();
+
+    private ArrayList<Tank> savedTanks = new ArrayList<Tank>();
+
+    private static int currentTankIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,10 +57,82 @@ public class MainActivity extends AppCompatActivity {
                 PopupMenu popupMenu = new PopupMenu(MainActivity.this, view);
                 popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
 
+                Menu popupMenuMenu = popupMenu.getMenu();
 
+                popupMenuItems.clear();
+                tanks.clear();
+
+                boolean needLoadTanks = (tanks.size() == 0) ? true : false;
+
+                for(int i = 0; i < popupMenuMenu.size(); i++) {
+                    popupMenuItems.add(popupMenuMenu.getItem(i));
+
+                    if(needLoadTanks)
+                        tanks.add(new Tank());
+                }
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        int itemIndex = popupMenuItems.indexOf(menuItem);
+                        currentTankIndex = itemIndex;
+
+                        setupText();
+
+                        Log.i(mainTag, "StartLevel: " + tanks.get(currentTankIndex).startLevel);
+
+                        return false;
+                    }
+                });
 
                 popupMenu.show();
             }
         });
+    }
+
+    private void setupTank() {
+        tanks.get(currentTankIndex).startLevel = Float.valueOf((((TextView) findViewById(R.id.startLevel)).getText().toString().isEmpty()) ? "0f" : ((TextView) findViewById(R.id.startLevel)).getText().toString());
+        tanks.get(currentTankIndex).endLevel = Float.valueOf((((TextView) findViewById(R.id.endLevel)).getText().toString().isEmpty()) ? "0f" : ((TextView) findViewById(R.id.endLevel)).getText().toString());
+
+        tanks.get(currentTankIndex).startVolume = Float.valueOf((((TextView) findViewById(R.id.startVolume)).getText().toString().isEmpty()) ? "0f" : ((TextView) findViewById(R.id.startVolume)).getText().toString());
+        tanks.get(currentTankIndex).endVolume = Float.valueOf((((TextView) findViewById(R.id.endVolume)).getText().toString().isEmpty()) ? "0f" : ((TextView) findViewById(R.id.endVolume)).getText().toString());
+
+        tanks.get(currentTankIndex).startWater = Float.valueOf((((TextView) findViewById(R.id.startWaterVolume)).getText().toString().isEmpty()) ? "0f" : ((TextView) findViewById(R.id.startWaterVolume)).getText().toString());
+        tanks.get(currentTankIndex).endWater = Float.valueOf((((TextView) findViewById(R.id.endWaterVolume)).getText().toString().isEmpty()) ? "0f" : ((TextView) findViewById(R.id.endWaterVolume)).getText().toString());
+
+        tanks.get(currentTankIndex).startFreeDensityCounter = Float.valueOf((((TextView) findViewById(R.id.startFreeDensityCounter)).getText().toString().isEmpty()) ? "0f" : ((TextView) findViewById(R.id.startFreeDensityCounter)).getText().toString());
+        tanks.get(currentTankIndex).endFreeDensityCounter = Float.valueOf((((TextView) findViewById(R.id.endFreeDensityCounter)).getText().toString().isEmpty()) ? "0f" : ((TextView) findViewById(R.id.endFreeDensityCounter)).getText().toString());
+
+        tanks.get(currentTankIndex).startFreeDensityCounterValue = Float.valueOf((((TextView) findViewById(R.id.startFreeDensityCounterValue)).getText().toString().isEmpty()) ? "0f" : ((TextView) findViewById(R.id.startFreeDensityCounterValue)).getText().toString());
+        tanks.get(currentTankIndex).endFreeDensityCounterValue = Float.valueOf((((TextView) findViewById(R.id.endFreeDensityCounterValue)).getText().toString().isEmpty()) ? "0f" : ((TextView) findViewById(R.id.endFreeDensityCounterValue)).getText().toString());
+
+        tanks.get(currentTankIndex).start15DensityCounterValue = Float.valueOf((((TextView) findViewById(R.id.start15DensityCounterValue)).getText().toString().isEmpty()) ? "0f" : ((TextView) findViewById(R.id.start15DensityCounterValue)).getText().toString());
+        tanks.get(currentTankIndex).end15DensityCounterValue = Float.valueOf((((TextView) findViewById(R.id.end15DensityCounterValue)).getText().toString().isEmpty()) ? "0f" : ((TextView) findViewById(R.id.end15DensityCounterValue)).getText().toString());
+
+        tanks.get(currentTankIndex).start20DensityCounterValue = Float.valueOf((((TextView) findViewById(R.id.start20DensityCounterValue)).getText().toString().isEmpty()) ? "0f" : ((TextView) findViewById(R.id.start20DensityCounterValue)).getText().toString());
+        tanks.get(currentTankIndex).end20DensityCounterValue = Float.valueOf((((TextView) findViewById(R.id.end20DensityCounterValue)).getText().toString().isEmpty()) ? "0f" : ((TextView) findViewById(R.id.end20DensityCounterValue)).getText().toString());
+    }
+
+    private void setupText() {
+        ((TextView) findViewById(R.id.startLevel)).setText(String.valueOf(tanks.get(currentTankIndex).startLevel));
+        ((TextView) findViewById(R.id.endLevel)).setText(String.valueOf(tanks.get(currentTankIndex).endLevel));
+
+        ((TextView) findViewById(R.id.startVolume)).setText(String.valueOf(tanks.get(currentTankIndex).startVolume));
+        ((TextView) findViewById(R.id.endVolume)).setText(String.valueOf(tanks.get(currentTankIndex).endVolume));
+
+        ((TextView) findViewById(R.id.startWaterVolume)).setText(String.valueOf(tanks.get(currentTankIndex).startWater));
+        ((TextView) findViewById(R.id.endWaterVolume)).setText(String.valueOf(tanks.get(currentTankIndex).endWater));
+
+        ((TextView) findViewById(R.id.startFreeDensityCounter)).setText(String.valueOf(tanks.get(currentTankIndex).startFreeDensityCounter));
+        ((TextView) findViewById(R.id.endFreeDensityCounter)).setText(String.valueOf(tanks.get(currentTankIndex).endFreeDensityCounter));
+
+        ((TextView) findViewById(R.id.startFreeDensityCounterValue)).setText(String.valueOf(tanks.get(currentTankIndex).startFreeDensityCounterValue));
+        ((TextView) findViewById(R.id.endFreeDensityCounterValue)).setText(String.valueOf(tanks.get(currentTankIndex).endFreeDensityCounterValue));
+
+        ((TextView) findViewById(R.id.start15DensityCounterValue)).setText(String.valueOf(tanks.get(currentTankIndex).start15DensityCounterValue));
+        ((TextView) findViewById(R.id.end15DensityCounterValue)).setText(String.valueOf(tanks.get(currentTankIndex).end15DensityCounterValue));
+
+        ((TextView) findViewById(R.id.start20DensityCounterValue)).setText(String.valueOf(tanks.get(currentTankIndex).start20DensityCounterValue));
+        ((TextView) findViewById(R.id.end20DensityCounterValue)).setText(String.valueOf(tanks.get(currentTankIndex).end20DensityCounterValue));
     }
 }
--- app/src/main/java/com/example/calculations/Tank.java
+++ app/src/main/java/com/example/calculations/Tank.java
@@ -2,7 +2,35 @@ package com.example.calculations;
 
 public class Tank {
 
-    public float level;
-    public float volume;
-    public float density;
+    public String name;
+
+    public float startLevel = 0;
+    public float endLevel = 0;
+
+    public float startVolume = 0;
+    public float endVolume = 0;
+
+    public float startWater = 0;
+    public float endWater = 0;
+
+    public float startVolumeWithoutWater;
+    public float endVolumeWithoutWater;
+
+    public float startFreeDensityCounter = 0;
+    public float startFreeDensityCounterValue = 0;
+
+    public float endFreeDensityCounter = 0;
+    public float endFreeDensityCounterValue = 0;
+
+    public float start15DensityCounterValue = 0;
+    public float end15DensityCounterValue = 0;
+
+    public float start20DensityCounterValue = 0;
+    public float end20DensityCounterValue = 0;
+
+    public float startTons;
+    public float endTons;
+    public float reception;
+    public float together;
+    public float difference;
 }
